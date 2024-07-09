1c7112fc839e2f383f733266e5d292348dd4073c
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2024-07-10 01:35:35 +0530

    Create a1_datatypes.js

---
+++ a1_datatypes.js
@@ -0,0 +1,18 @@
+
+Return type of variables in JavaScript
+1) Primitive Datatypes
+Primitive data types are the most basic data types in JavaScript. They are immutable, meaning that their values cannot be changed once created. There are seven primitive data types in JavaScript:
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+Non-primitive data types, also known as reference types, are objects and arrays that can store collections of values and more complex entities. These types are mutable, meaning their contents can be changed. 
+  They are also accessed by reference, meaning that the variable holds a reference to the memory location where the data is stored.
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
